de34392b9bbe4f0095a1e026fe59f0e8ccef7247
Anne Culpepper <anne.culpepper@gmail.com>
2020-09-20 17:56:16 -0500

    imported RouterModule and defined url paths for each component

--- angular-shopping-router/src/app/app.module.ts
+++ angular-shopping-router/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ShoppingCartComponent,
-    CheckoutComponent
-  ],
+  declarations: [AppComponent, ShoppingCartComponent, CheckoutComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'checkout', component: CheckoutComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
