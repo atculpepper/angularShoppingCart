fffdeb73bccec4b148cf2488285664a9414f4a24
Anne Culpepper <anne.culpepper@gmail.com>
2020-09-20 17:34:27 -0500

    created checkout and shopping-cart modules, added them both to app component html

--- angular-shopping-router/src/app/app.module.ts
+++ angular-shopping-router/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule
