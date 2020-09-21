c49fac9e8361f42550a97fc5cf869a7fe8932cf5
Anne Culpepper <anne.culpepper@gmail.com>
2020-09-21 18:23:04 -0500

    changed the values within product item component to reflect products provided by Product Service

--- angular-shopping-router/src/app/services/product.service.ts
+++ angular-shopping-router/src/app/services/product.service.ts
@@ -8,12 +8,12 @@ export class ProductService {
   //products is a property
   products: Product[] = [
     new Product(1, 'Stylo Un', 'This is an excellent fountain pen', 60),
-    new Product(2, 'Stylo Un', 'This is an excellent fountain pen', 85),
-    new Product(3, 'Stylo Un', 'This is an excellent fountain pen', 70),
-    new Product(4, 'Stylo Un', 'This is an excellent fountain pen', 45),
-    new Product(5, 'Stylo Un', 'This is an excellent fountain pen', 50),
-    new Product(6, 'Stylo Un', 'This is an excellent fountain pen', 32),
-    new Product(7, 'Stylo Un', 'This is an excellent fountain pen', 78),
+    new Product(2, 'Stylo Deux', 'A customer favorite', 85),
+    new Product(3, 'Stylo Trois', 'Write with class', 70),
+    new Product(4, 'Stylo Quatre', 'Best value', 45),
+    new Product(5, 'Stylo Cinq', 'Executive Pen', 50),
+    new Product(6, 'Stylo Six', 'Budget pen', 32),
+    new Product(7, 'Stylo Sept', 'Crowd pleaser', 78),
   ];
 
   constructor() {}
