89a30d326a455aed05f93d00031bd91e84813c8d
Anne Culpepper <anne.culpepper@gmail.com>
2020-09-20 20:03:56 -0500

    updating template in app component to include links to different page views

--- angular-shopping-router/src/app/app.component.ts
+++ angular-shopping-router/src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <a routerLinkActive="active" routerLink="/shopping-cart">Shopping Cart</a>
+      |
+
+      <a routerLinkActive="active" routerLink="/checkout">Checkout</a> |
+
+      <router-outlet></router-outlet>
+    </div>
+  `,
+
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-shopping-router';
