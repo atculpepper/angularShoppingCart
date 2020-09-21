74704f2047a8e020f2feb8863b6284a1496a7840
Anne Culpepper <anne.culpepper@gmail.com>
2020-09-21 09:48:04 -0500

    downloaded bootstrap and running it from node modules. Hard coded some images in to shopping component. Changed class of update cart button on shopping cart. Imported FormsModule into app module, registered it with NgModule.

--- angular-shopping-router/src/app/app.module.ts
+++ angular-shopping-router/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ShoppingCartComponent, CheckoutComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'checkout', component: CheckoutComponent },
